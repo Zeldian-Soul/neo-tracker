954398186c14826b3d3c7d0e39936218720824ff
Abhiram <abhiramav741915@gmail.com>
2026-07-30 18:44:45 +0530

    Add vercel.json for multi-service full-stack deployment

--- api/index.js
+++ api/index.js
@@ -1,3 +1,5 @@
-// C:\Projects\neo-tracker\api\index.js
+// api/index.js
+// Connects Vercel Serverless Functions directly to your Express app
 const app = require('../backend/server.js');
+
 module.exports = app;
